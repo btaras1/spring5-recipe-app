c3249da40708325720adbe09cf353667214f9f9e
btaras1 <taras.bruno@gmail.com>
2022-06-01 10:42:45 +0200

    Modified data.sql and index controller

--- src/main/java/com/guru/receipts/controllers/IndexController.java
+++ src/main/java/com/guru/receipts/controllers/IndexController.java
@@ -1,13 +1,31 @@
 package com.guru.receipts.controllers;
 
+import com.guru.receipts.domain.Category;
+import com.guru.receipts.domain.Recipe;
+import com.guru.receipts.domain.UnitOfMeasure;
+import com.guru.receipts.repositories.CategoryRepository;
+import com.guru.receipts.repositories.UnitOfMeasureRepository;
+import com.guru.receipts.services.RecipeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    private final RecipeService recipeService;
+
+    public IndexController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
     @RequestMapping({"","/","/index"})
-    public String getIndexPage(){
+    public String getIndexPage(Model model){
+
+        model.addAttribute("recipes", recipeService.getRecipes());
+
         return "index";
     }
 }
