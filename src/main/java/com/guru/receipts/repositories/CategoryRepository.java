ff9c25a62e3eb714e36fa794c9828efe4cc1c942
btaras1 <taras.bruno@gmail.com>
2022-06-01 10:40:56 +0200

    Added category and uom repo

--- src/main/java/com/guru/receipts/repositories/CategoryRepository.java
+++ src/main/java/com/guru/receipts/repositories/CategoryRepository.java
@@ -3,5 +3,9 @@ package com.guru.receipts.repositories;
 import com.guru.receipts.domain.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+    Optional<Category> findByDescription(String description);
 }
